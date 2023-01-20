e65c9eaf8f6005b2bc118f35dc60c98ccd02c9ce
OlgaTeg <tegza.o@yandex.ru>
2023-01-20 09:12:28 +0300

    Initial Commit

---
+++ SliderGameApp/SliderGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameAppApp.swift
+//  SliderGameApp
+//
+//  Created by Olga Tegza on 20.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
